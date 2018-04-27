8780c61aff7cd0cbd50724f50c901f7f9e568e31
wuting <reruin@gmail.com>
2018-04-27 23:25:57 +0800

    signed

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,8 @@ unzip sharelist.zip
 cd /app/sharelist-master
 npm install
 
-mkdir /app/sharelist-master/cache
-
 if echo "$CONFIG" | grep -q -i "^http"; then
-  wget --no-check-certificate $CONFIG_URL -O /app/sharelist-master/cache/config.json
+  wget --no-check-certificate $CONFIG -O /app/sharelist-master/cache/config.json
 else
   echo -e "$CONFIG" > /app/sharelist-master/cache/config.json
 fi
